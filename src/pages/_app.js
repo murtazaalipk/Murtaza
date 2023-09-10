b193f9c63722322ba70811d88af5d1354ad514e2
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-09-11 02:31:53 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
+import Header from '@/components/header';
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+// pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Header/>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+export default App;
