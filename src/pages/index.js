140d527058349b49b288d0247d69386843e06867
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-09-11 03:56:03 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-
 import { Inter } from "next/font/google";
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,15 +8,26 @@ export default function Home() {
   return (
     <main className=" m-6">
       <div className="flex justify-start gap-20">
-       <div className="flex flex-col gap-10">
-        <h1 className=" text-7xl font-bold font-mono">Hi,</h1>
-        <h1 className=" text-7xl font-bold font-mono">I'm Syed Murtaza Ali</h1>
-        <h3 className=" text-4xl font-mono">frontend Developer</h3>
-       </div>
-       <div>
-        <img className=" border-8 border-[#c6f6d5] border-solid h-96 rounded-full" src="../mypic.jpg" />
-       </div>
-       </div>
+        <div className="flex flex-col gap-10">
+          <h1 className=" text-7xl font-bold font-mono">Hi,</h1>
+          <h1 className=" text-7xl font-bold font-mono">
+            I'm Syed Murtaza Ali
+          </h1>
+          <h3 className=" text-4xl font-mono">frontend Developer</h3>
+        </div>
+        <div>
+          <img
+            className=" border-8 border-[#c6f6d5] border-solid h-96 rounded-full"
+            src="../mypic.jpg"
+          />
+        </div>
+      </div>
+      <div className=" -mt-20">
+
+      <Link href={'./project'} > <Button  colorScheme='green' variant='solid'>
+    See my Projects
+  </Button></Link>
+      </div>
     </main>
   );
 }
