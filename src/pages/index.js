0fc5d47d4de5dcc58b4a85308a48ddb2fe2afa05
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-09-11 03:27:03 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
 
 import { Inter } from "next/font/google";
 
-
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className=" m-6">
-       I am Home
+      <div className="flex justify-start gap-20">
+       <div className="flex flex-col gap-10">
+        <h1 className=" text-7xl font-bold font-mono">Hi,</h1>
+        <h1 className=" text-7xl font-bold font-mono">I'm Syed Murtaza Ali</h1>
+        <h3 className=" text-4xl font-mono">frontend Developer</h3>
+       </div>
+       <div>
+        <img className=" border-8 border-[#c6f6d5] border-solid h-96 rounded-full" src="../mypic.jpg" />
+       </div>
+       </div>
     </main>
   );
 }
