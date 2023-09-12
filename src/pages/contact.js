b1c884e79896d885a7630a7ce1515b2e758ec01a
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-09-12 21:45:28 +0500

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,21 @@
+import Link from "next/link"
+
 
 export default function Contact() {
     return (
-      <main className=" m-6">
-  I am Contact
+      <main className=" ">
+          <div className="flex gap-40 justify-center">
+            <div>
+              <h1 className="font-mono text-2xl font-bold mt-10 text-center" >I'm Here For you</h1>
+              <div className="flex flex-col gap-8" >
+                <div><p className="text-gray-600 font-mono mt-10" >Stay connected with me! I value your engagement <br/>and would love to hear from you. Whether you have <br/>a question, feedback, or just want to say hello,<br/> you can reach me through multiple channels</p></div>
+                <div className="flex gap-10 items-center" > <img className="h-10" src="../phone.png" /><Link href={'tel:+923343615281'} > <p className="text-yellow-500 font-mono font-bold text-xl" >+923343615281</p></Link> </div>
+                <div className="flex gap-10 items-center"> <img className="h-10" src="../mail.png" /> <Link href={'mailto:syedmurtazaaliofficial@gmail.com'}><p className="text-yellow-500 font-mono font-bold text-xl">syedmurtazaaliofficial@gmail.com</p></Link></div>
+                <div className="flex gap-10 items-center"> <img className="h-10" src="../linkedin.png" /><Link href={'https://www.linkedin.com/in/murtazaalipk/'} ><p className="text-yellow-500 text-xl font-mono mt-2 font-bold">murtazaalipk</p></Link></div>
+              </div>
+            </div>
+            <img className="h-[32rem]" src="../contact.jpg" />
+          </div>
       </main>
     )
   }
