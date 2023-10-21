9428b8093db6de60e69f896639ad86fd6ce36332
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-10-21 13:57:25 +0500

    Update project.js

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react';
+
 export default function Project() {
+  const [Data, setData] = useState({});
+
+  useEffect(() => {
+    // Fetch data from your API route
+    fetch('/api/projects')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching profile data:', error));
+  }, []);
   return (
     <main className=" m-6">
        I am Projects
+       <h1>Project 1</h1>
+{       <p>{Data.data[0].name}</p>}
+       <h1>Project 2</h1>
+{       <p>{Data.data[1].name}</p>}
     </main>
   )
 }
